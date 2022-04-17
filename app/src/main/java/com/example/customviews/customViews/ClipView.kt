5e574f3d1d82370a0b2b4642d9cdc81f18e5cf07
PSWang <pswang1991@gmail.com>
2022-04-17 16:37:06 +0800

    Practice canvas clip and camera.

--- app/src/main/java/com/example/customviews/customViews/ClipView.kt
+++ app/src/main/java/com/example/customviews/customViews/ClipView.kt
@@ -1,14 +1,13 @@
 package com.example.customviews.customViews
 
 import android.content.Context
+import android.graphics.Camera
 import android.graphics.Canvas
 import android.graphics.Paint
-import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 import com.example.customviews.utils.dp2px
 import com.example.customviews.utils.getChihuahua
-import kotlin.math.min
 
 /**
  * Created by PS Wang on 2022/4/15
@@ -16,26 +15,65 @@ import kotlin.math.min
 
 
 private val photoOrigin = Point(100.dp2px, 150.dp2px)
-private val circleXShift = 35.dp2px
+private const val cameraRotateX = 30f
+private const val canvasRotate = -15f
+private const val clipExtension = 200
 
 class ClipView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val clipPath = Path()
 
     private val chihuahua = getChihuahua(resources, 150.dp2px.toInt())
 
-    private val radius = min(chihuahua.width, chihuahua.height).toFloat() / 2f
-    private val origin = Point(photoOrigin.x + radius + circleXShift, photoOrigin.y + radius)
+    private val camera = Camera().apply {
+        rotateX(cameraRotateX)
+        setLocation(0f, 0f, -5 * resources.displayMetrics.density)
+    }
 
     override fun onDraw(canvas: Canvas) {
 
-        paint.alpha = 125
+        canvas.save()
+        canvas.translate(
+            (photoOrigin.x + chihuahua.width / 2),
+            (photoOrigin.y + chihuahua.height / 2)
+        )
+        canvas.rotate(canvasRotate)
+        camera.applyToCanvas(canvas)
+        canvas.clipRect(
+            -chihuahua.width / 2 - clipExtension,
+            0,
+            chihuahua.width / 2 + clipExtension,
+            chihuahua.height / 2 + clipExtension
+        )
+        canvas.rotate(-canvasRotate)
+        canvas.translate(
+            -(photoOrigin.x + chihuahua.width / 2),
+            -(photoOrigin.y + chihuahua.height / 2)
+        )
         canvas.drawBitmap(chihuahua, photoOrigin.x, photoOrigin.y, paint)
+        canvas.restore()
 
-        clipPath.addCircle(origin.x, origin.y, radius, Path.Direction.CW)
-        canvas.clipPath(clipPath)
-        paint.alpha = 255
+
+        canvas.save()
+        canvas.translate(
+            (photoOrigin.x + chihuahua.width / 2),
+            (photoOrigin.y + chihuahua.height / 2)
+        )
+        canvas.rotate(canvasRotate)
+//        camera.rotateX(-2 * cameraRotateX)
+//        camera.applyToCanvas(canvas)
+        canvas.clipRect(
+            -chihuahua.width / 2 - clipExtension,
+            -chihuahua.height / 2 - clipExtension,
+            chihuahua.width / 2 + clipExtension,
+            0
+        )
+        canvas.rotate(-canvasRotate)
+        canvas.translate(
+            -(photoOrigin.x + chihuahua.width / 2),
+            -(photoOrigin.y + chihuahua.height / 2)
+        )
         canvas.drawBitmap(chihuahua, photoOrigin.x, photoOrigin.y, paint)
+        canvas.restore()
     }
 }
